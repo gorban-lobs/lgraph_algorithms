87623663dc35e3127aee605486a76f8001fe9e9a
gor_lobs <nikita.lobosov.wa@yandex.ru>
2020-05-06 16:04:01 +0300

    Fix is_determinated algorithm

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -9,36 +9,39 @@ def stack_processing(trace_stack, cur_trace):
     )
     for trace in traces:
         trace_type = trace['trace_type']
-        if cur_trace[trace_type]:
-            if cur_trace[trace_type][0] == trace['open']:
-                trace_stack[trace_type].append(cur_trace[trace_type])
-            elif cur_trace[trace_type][0] == trace['closed']:
-                if len(trace_stack[trace_type]) == 0 or \
-                        trace_stack[trace_type][-1][1] != \
-                        cur_trace[trace_type][1]:
-                    return False
-                else:
-                    trace_stack[trace_type].pop()
+        # if cur_trace[trace_type][0]:
+        if cur_trace[trace_type][0] == trace['open']:
+            trace_stack[trace_type].append(cur_trace[trace_type])
+        elif cur_trace[trace_type][0] == trace['closed']:
+            if len(trace_stack[trace_type]) == 0 or \
+                    trace_stack[trace_type][-1][1] != \
+                    cur_trace[trace_type][1]:
+                return False
+            else:
+                trace_stack[trace_type].pop()
     return True
 
 
 def rest_direct(vertex, trace_stack, passed_edges, determ_vertexes, finals):
     fst_label_set = set()
-    determ_vertexes[vertex.name] = {'is_determ': True, 'fst_labels_set': None}
+    if vertex.name not in determ_vertexes:
+        determ_vertexes[vertex.name] = {'is_determ': True, 
+                                        'fst_labels_set': set()}
     for edge in vertex.edges:
         new_passed_edges = passed_edges.copy()
         if (edge.beg.name, edge.end.name) in passed_edges:
-            return
+            # print('return', vertex.name)
+            continue
         else:
             new_passed_edges.add((edge.beg.name, edge.end.name))
- 
+
         if edge.end in finals:
             # TODO make smth with finals
             fst_label_set.add('eps')
             continue
         direct_set = set()
         new_stack = trace_stack.copy()
-        if stack_processing(new_stack, 
+        if stack_processing(new_stack,
                             {'round': edge.round_trace, 
                              'square': edge.round_trace}):
             if edge.label:
@@ -59,9 +62,12 @@ def rest_direct(vertex, trace_stack, passed_edges, determ_vertexes, finals):
                     direct_set.add(label)
         fst_label_size = len(fst_label_set)
         fst_label_set = fst_label_set.union(direct_set)
+        # print('fst_label_set:', fst_label_set)
         if len(fst_label_set) != fst_label_size + len(direct_set):
             determ_vertexes[vertex.name]['is_determ'] = False
-        determ_vertexes[vertex.name]['fst_labels_set'] = fst_label_set
+        # print((edge.beg.name, edge.end.name), fst_label_set)
+    determ_vertexes[vertex.name]['fst_labels_set'] = \
+        determ_vertexes[vertex.name]['fst_labels_set'].union(fst_label_set)
     return fst_label_set
 
 
