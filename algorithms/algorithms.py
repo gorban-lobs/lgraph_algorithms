bb024bd840b9cdcbe4b2874303e643818f1b8c56
gor_lobs <nikita.lobosov.wa@yandex.ru>
2020-05-17 23:59:25 +0300

    Add function for creating conjugation L-graph

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,5 +1,55 @@
 from copy import deepcopy
-from lgraph import LGraph
+from lgraph import LGraph, Vertex, Edge
+
+
+class Edge(Edge):
+    def __init__(self,
+                 beg_vertex: Vertex,
+                 end_vertex: Vertex,
+                 label='',
+                 round_trace=('', 0),
+                 square_trace=('', 0)):
+        self.__beg = beg_vertex
+        self.__end = end_vertex
+        self.__label = label
+        if isinstance(round_trace, str):
+            round_trace = (round_trace, 0)
+        if isinstance(square_trace, str):
+            square_trace = (square_trace, 0)
+
+        if not (isinstance(round_trace, tuple)
+                and isinstance(square_trace, tuple)
+                and len(round_trace) == len(square_trace) == 2
+                and isinstance(round_trace[1], int)
+                and isinstance(square_trace[1], int)):
+            raise ValueError(
+                'Bracket trace should be a tuple object '
+                'with a bracket as first element and its number as a second.')
+
+        self.__round_trace = round_trace
+        self.__square_trace = square_trace
+
+
+class ConjLGraph(LGraph):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def add_edge(self,
+                 beg_vtx_name: str,
+                 end_vtx_name: str,
+                 label='',
+                 round_trace=('', 0),
+                 square_trace=('', 0)):
+        if label not in self.alphabet:
+            raise ValueError(f'Edge label "{label}"" is not from the alphabet.')
+
+        beg_vtx = self.get_vertex(beg_vtx_name)
+        end_vtx = self.get_vertex(end_vtx_name)
+        edg = Edge(
+            beg_vtx, end_vtx, label=label,
+            round_trace=round_trace, square_trace=square_trace)
+        self._LGraph__edges.add(edg)
+        beg_vtx.add_edge(edg)
 
 
 def stack_processing(trace_stack, cur_trace):
@@ -30,7 +80,6 @@ def rest_direct(vertex, trace_stack, passed_edges, determ_vertices, finals,
     for edge in vertex.edges:
         new_passed_edges = passed_edges.copy()
         cur_edge = (edge.beg.name, edge.end.name)
-        # print(f'{cur_edge} | {new_passed_edges} | {trace_stack}')
         if cur_edge in new_passed_edges and \
                 new_passed_edges[cur_edge] == cycle_iterations:
             continue
@@ -98,3 +147,65 @@ def is_determinated(direct_dict):
         if not vert['is_determ']:
             return False
     return True
+
+
+def rest_conjugation(vertex1, vertex2,
+                     lgraph1, lgraph2,
+                     conj_graph, passed_edges):
+    for edge1 in vertex1.edges:
+        success_flag = False
+        for edge2 in vertex2.edges:
+            if edge1.label == edge2.label:
+                success_flag = True
+                end_vertex1, end_vertex2 = edge1.end, edge2.end
+                edge_beg = f'{vertex1.name}|{vertex2.name}'
+                edge_end = f'{end_vertex1.name}|{end_vertex2.name}'
+                if (edge_beg, edge_end) not in passed_edges:
+                    edge_round_trace = (
+                        '(|'
+                        f'{edge1.round_trace[0]},{edge1.round_trace[1]}|'
+                        f'{edge2.round_trace[0]},{edge2.round_trace[1]}', 0)
+                    edge_square_trace = (
+                        '[|'
+                        f'{edge1.square_trace[0]},{edge1.square_trace[1]}|'
+                        f'{edge2.square_trace[0]},{edge2.square_trace[1]}', 0)
+                    if edge_end not in [vert.name 
+                                        for vert in conj_graph.vertexes]:
+                        conj_graph.add_vertex(edge_end)
+                    conj_graph.add_edge(edge_beg, edge_end,
+                                        label=edge1.label,
+                                        round_trace=edge_round_trace,
+                                        square_trace=edge_square_trace)
+                    print(edge_beg, edge_end)
+                    passed_edges.add((edge_beg, edge_end))
+                    if not rest_conjugation(end_vertex1, end_vertex2,
+                                            lgraph1, lgraph2,
+                                            conj_graph, passed_edges):
+                        return False
+                else:
+                    success_flag = True
+        if not success_flag:
+            return False
+    return True
+
+
+def create_conjugation_lgraph(lgraph1, lgraph2):
+    conj_graph = ConjLGraph()
+    passed_edges = set()
+    success_flag = True
+    for initial1 in lgraph1.initials:
+        for initial2 in lgraph2.initials:
+            for edge1 in initial1.edges:
+                for edge2 in initial2.edges:
+                    vertex1, vertex2 = edge1.end, edge2.end
+                    conj_graph.add_vertex(f'{vertex1.name}|{vertex2.name}')
+                    success_flag = rest_conjugation(vertex1, vertex2,
+                                                    lgraph1, lgraph2,
+                                                    conj_graph, passed_edges) \
+                                   or success_flag
+    if success_flag:
+        print('Creation of conjugation L-graph: Success')
+        return conj_graph
+    else:
+        print('Creation of conjugation L-graph: Failure')
+        return None
